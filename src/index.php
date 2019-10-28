9aedc51d5f1536e1517ddf1ced216d4025774ac1
Stephen Beck <beckstep@amazon.com>
2019-10-28 15:23:03 -0500

    Adding some more friendly language

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Acxiom Immersion Day</h1>
                 <h2>Great Giveaways and Takeaways</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <p>Everybody's a winner. Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
